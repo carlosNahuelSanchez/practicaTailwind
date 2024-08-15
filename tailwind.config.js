4447909fa82976997c5b7c753546d8fc86e3c897
Nahuel <nahuelcarlos150@gmail.com>
2024-08-15 16:43:29 -0300

    LOL

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      boxShadow: {
+        'blue-glow': '0 4px 6px rgba(37, 99, 235, 0.5)', 
+        'red-glow': '0 4px 6px rgba(220, 38, 38, 0.5)',
+        'green-glow': '0 4px 6px rgba(34, 197, 94, 0.5)'
+      },
+    },
+  },
+  plugins: [],
+}
+
+
